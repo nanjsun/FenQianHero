64d16362019fd63c10012a55a753a87cb999631c
Nanj sun <279956327@qq.com>
2018-01-29 15:23:21 +0800

    baidu ocr api ok

--- src/main/java/com/fenqian/LetUsGetMoney.java
+++ src/main/java/com/fenqian/LetUsGetMoney.java
@@ -3,12 +3,12 @@ package com.fenqian;
 import com.fenqian.click.HandIn;
 import com.fenqian.image.ScreenShotImage;
 import com.fenqian.ocr.ali.AliOCR;
+import com.fenqian.ocr.baidu.BaiduOCR;
 import com.fenqian.search.SearchQuestion;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.concurrent.*;
 
 /**
  * @author nanj
@@ -16,7 +16,7 @@ import java.util.concurrent.*;
 public class LetUsGetMoney {
     private final int NUM_OF_ANSWERS = 2;
     private final String QUESTION_FLAG="?";
-    private String[] questionAndAnswer = new String[4];
+    private String[] questionAndOptions = new String[4];
     private String[] answers = new String[3];
     private String question;
     private long startTime;
@@ -26,65 +26,9 @@ public class LetUsGetMoney {
     private int width = 600;
     private int height = 360;
     private String lastQuestionKeyWord = "Nothing";
+    private String ocrSupplier = "baidu";
 
-    public void start() {
-
-
-        boolean imageValid = false;
-        ScreenShotImage screenShotImage = new ScreenShotImage();
-        BufferedImage bufferedImage = screenShotImage.getBufferedImage(x, y, width, height);
-        if (screenShotImage.isValidImage()){
-            imageValid = true;
-        }
-
-        while (!imageValid){
-            bufferedImage = screenShotImage.getBufferedImage(x, y, width, height);
-            if(screenShotImage.isValidImage()){
-                imageValid = true;
-            }
-        }
-
-        Long beginOfImageDectect = System.currentTimeMillis();
-        AliOCR aliOCR = new AliOCR();
-
-        question = aliOCR.parseReslut(aliOCR.callAliOcrAPI(aliOCR.getBase64Code(bufferedImage)))[0];
-
-        String originalWords = new TessOCR().getOCRByBufferedImage(bufferedImage);
-
-        System.out.println("----->Original word:");
-        System.out.println(originalWords);
-        System.out.println("----->Image resolve time:");
-        System.out.println(System.currentTimeMillis() - beginOfImageDectect);
-
-        //detect if this image is valid
-        if (!originalWords.contains(QUESTION_FLAG)) {
-            System.out.println("There is no '?' in image");
-        }
-        if (originalWords.contains(lastQuestionKeyWord)) {
-            System.out.println("same question");
-        }
-
-        if (originalWords.contains(QUESTION_FLAG) && !originalWords.contains(lastQuestionKeyWord)) {
-
-            Information information = new Information(originalWords);
-            lastQuestionKeyWord = information.getQuestionKeyWord();
-            this.question = information.getQuestion();
-            this.answers = information.getAns();
-
-            System.out.println("Question is:");
-            System.out.println(question);
-            System.out.println("Answer is :");
-            int numberOfAns = answers.length;
-            for(int i = 0; i < numberOfAns; i++){
-                System.out.println((i + 1) + ":" + answers[i]);
-            }
-
-            beginSearch();
-        }
-    }
-
-
-    public void startWithAliOCR(){
+    public void startWithOnlineOCR() throws IOException{
         boolean imageValid = false;
         int recaptureCount = 0;
         ScreenShotImage screenShotImage = new ScreenShotImage();
@@ -109,17 +53,29 @@ public class LetUsGetMoney {
         }
         startTime = System.currentTimeMillis();
 
-        Long beginOfImageDectect = System.currentTimeMillis();
-        AliOCR aliOCR = new AliOCR();
-        questionAndAnswer = aliOCR.parseReslut(aliOCR.callAliOcrAPI(aliOCR.getBase64Code(bufferedImage)));
-        question = questionAndAnswer[0];
-        System.out.println("questionAndAnswer size:"  + questionAndAnswer.length);
-
-        for(int i = 0; i < questionAndAnswer.length -1 ; i ++){
-            this.answers[i] = this.questionAndAnswer[i +1];
+        if("baidu".equals(ocrSupplier)){
+            BaiduOCR baiduOCR = new BaiduOCR();
+            baiduOCR.parseImage(bufferedImage);
+            System.out.println("print the result from baidu");
+            questionAndOptions = baiduOCR.getQuestionAndOptions();
+            for (int i = 0; i < 4; i ++){
+                System.out.println("baidu:" + questionAndOptions[i]);
+            }
+        } else {
+            AliOCR aliOCR = new AliOCR();
+            questionAndOptions = aliOCR.parseReslut(aliOCR.callAliOcrAPI(aliOCR.getBase64Code(bufferedImage)));
         }
 
-        SearchQuestion searchQuestion = new SearchQuestion(questionAndAnswer, answers);
+
+
+        question = questionAndOptions[0];
+        System.out.println("questionAndOptions size:"  + questionAndOptions.length);
+
+        for(int i = 0; i < questionAndOptions.length -1 ; i ++){
+            this.answers[i] = this.questionAndOptions[i +1];
+        }
+
+        SearchQuestion searchQuestion = new SearchQuestion(questionAndOptions, answers);
         try {
             searchQuestion.search(question);
         } catch (IOException e){
@@ -144,90 +100,4 @@ public class LetUsGetMoney {
         System.out.println("finalTime :" + (System.currentTimeMillis() - startTime));
 
     }
-
-    private void beginSearch(){
-        long countQuestion = 1;
-        int maxIndex = 0;
-
-        Search serachQuestion = new Search(question);
-
-        ArrayBlockingQueue<Runnable> workQuenue = new ArrayBlockingQueue<Runnable>(5);
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5,10,
-                200, TimeUnit.MINUTES,workQuenue, Executors.defaultThreadFactory(),new RejectedExecutionHandlerForSearch());
-
-        FutureTask<Long> futureQuestion = new FutureTask<Long>(new SearchAndOpen(question));
-        new Thread(futureQuestion).start();
-        for (int i = 0; i < NUM_OF_ANSWERS; i++) {
-//            searchQuestionAndAnswer[i] = new Search(question + " " + answers[i]);
-//            searchAnswers[i] = new Search(answers[i]);
-
-//            futureQA[i] = new FutureTask<Long>(searchQuestionAndAnswer[i]);
-//            futureAnswers[i] = new FutureTask<Long>(searchAnswers[i]);
-//            new Thread(futureQA[i]).start();
-//            new Thread(futureAnswers[i]).start();
-        }
-        try {
-            while (!futureQuestion.isDone()) {}
-            countQuestion = futureQuestion.get();
-            for (int i = 0; i < NUM_OF_ANSWERS; i++) {
-//                while (!futureQA[i].isDone()) {}
-//                countQuestionAndAnswer[i] = futureQA[i].get();
-//                while (!futureAnswers[i].isDone()) {}
-//                countAnswer[i] = futureAnswers[i].get();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        float[] ans = new float[NUM_OF_ANSWERS];
-        for (int i = 0; i < NUM_OF_ANSWERS; i++) {
-//            ans[i] = (float)countQuestionAndAnswer[i] / (float)(countQuestion * countAnswer[i]);
-            maxIndex = (ans[i]>ans[maxIndex] ) ? i : maxIndex;
-        }
-        //根据pmi值进行打印搜索结果
-        int[] rank=rank(ans);
-        for (int i : rank) {
-            System.out.print(answers[i]);
-//            System.out.print(" countQuestionAndAnswer:"+countQuestionAndAnswer[i]);
-//            System.out.print(" countAnswer:"+countAnswer[i]);
-            System.out.println(" ans:"+ans[i]);
-        }
-
-        System.out.println("--------最终结果-------");
-        System.out.println(answers[maxIndex]);
-        endTime = System.currentTimeMillis();
-        float excTime = (float) (endTime - startTime) / 1000;
-
-        System.out.println("执行时间：" + excTime + "s");
-
-
-    }
-
-    private int[] rank(float[] floats){
-        int[] rank=new int[NUM_OF_ANSWERS];
-        float[] f= Arrays.copyOf(floats,3);
-        Arrays.sort(f);
-        for (int i = 0; i < NUM_OF_ANSWERS; i++) {
-            for (int j = 0; j < NUM_OF_ANSWERS; j++) {
-                if(f[i]==floats[j]){
-                    rank[i]=j;
-                }
-            }
-        }
-        return rank;
-
-    }
-
-
 }
-
-
-/*
-
-    private static void begainSearch(){
-
-
-    }
-}
-*/
